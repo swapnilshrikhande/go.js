13b62a0e77e7a1b1bb88d7a7ab81c5145023c595
Swapnil Shrikhande <swapnil.shrikhande@roundcorner.com>
2016-02-14 10:54:35 +0530

    Added complete view code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # go.js
-Go implementation in javascript
+Go implementation in javascript.
+NOTE : Primary aim of this project is for teaching and learning to develop Javascript application.
+Thus the current implementation support only subset of full Go game rules.
+Please do not use for learning Go or professional games.
+Use http://playgo.to/iwtg/en/ to learn Go and https://online-go.com instead to play.
+
+#TODO
+1. Ko rule
+2. Handicap rule
+3. Point Calculation
+4. Komi
--- go.js
+++ go.js
@@ -7,33 +7,35 @@ gogame.DEFAULT_BOARDSIZE = 9;
 gogame.EMPTY = 0;
 gogame.BLACK = 1;
 gogame.WHITE = 2;
-gogame.GRID_SIZE = 40;
+gogame.INTERS_SIZE  = 40;
+gogame.BOARD_MARGIN = 10;
 
 //Variables
 gogame.board = [];
 
 //simple html templates
-gogame.INTERSECTION_TEMPLATE = '<div class="intersection"></div>';
+gogame.INTERSECTION_TEMPLATE =
+         '<div class="empty intersection" data-row="1" data-col="1">'
+        +'      <div class="vertical-line" ></div>'
+        +'      <div class="horizontal-line"></div>'
+        +'      <div class="stone"></div>'
+        +'</div>';
 
 //main function
 $.fn.go = function(options) {
-
-
     //initializing go game
     console.log("Initializing Go Game ",this);
     console.log("Options ",options);
     if( options ){
         gogame["size"] = options.boardsize;
-        gogame["boardelem"] = this;
     }
-
+    gogame["boardDomElem"] = this;
     gogame.initialize();
 
     return this;
 };
 
 gogame.initialize = function(){
-    var boardDomElem = gogame["boardelem"];
 
     this.current_color = gogame.BLACK;
     this.size = this.size || gogame.DEFAULT_BOARDSIZE;
@@ -41,9 +43,21 @@ gogame.initialize = function(){
     this.last_move_passed = false;
     this.in_atari = false;
     this.attempted_suicide = false;
+    this.initBoardView();
 
     console.log("Here This Is =",this);
 };
+
+gogame.playEventHandler = function(event){
+    var cell   = this || event.target;
+    var cellJQ = $(cell);
+    var row    = parseInt( cellJQ.attr("row"),10 );
+    var col    = parseInt( cellJQ.attr("col"),10 );
+    gogame.play(row,col);
+    gogame.refreshBoard();
+};
+
+
 
 gogame.createBoard = function(size) {
     var board = [];
@@ -59,6 +73,77 @@ gogame.createBoard = function(size) {
 
     return board;
 };
+
+gogame.initBoardView = function(){
+    //to start with all Intersections will be empty
+    //as set in the template
+    var size = this.size || gogame.DEFAULT_BOARDSIZE;
+    var newElementsArr = [];
+    var newElem;
+
+    for (var row=0 ; row < size ; ++row ) {
+        for (var col=0; col < size ; ++col ) {
+            newElem = $(gogame.INTERSECTION_TEMPLATE);
+            positionCss = gogame.getCellPositionCss(row,col);
+            newElem.attr("row",row)
+                   .attr("col",col)
+                   .css(positionCss);
+            newElementsArr.push(newElem);
+        }
+    }
+    console.log('newElementsArr',newElementsArr);
+
+    var boardHeight = size*gogame.INTERS_SIZE+5;
+    var boardWidth  = boardHeight;
+
+    $( this.boardDomElem ).html("")
+                          .append(newElementsArr)
+                          .on("click",".intersection",this.playEventHandler)
+                          .css("height",boardHeight)
+                          .css("width",boardWidth)
+                          .css("background-color","#ff9900");
+}
+
+gogame.getCellPositionCss = function(row,col){
+
+    var topPx   = row*gogame.INTERS_SIZE + gogame.BOARD_MARGIN;
+    var leftPx  = col*gogame.INTERS_SIZE + gogame.BOARD_MARGIN;
+    return { top:topPx+'px', left:leftPx+'px' };
+};
+
+gogame.refreshBoard = function(){
+
+    var emptyIntersections = [];
+    var whiteIntersections = [];
+    var blackIntersectons  = [];
+    var currentCellDomElem;
+    var cellColor;
+
+    for (var row=0 ; row < this.size ; ++row ) {
+        for (var col=0; col < this.size ; ++col ) {
+
+            currentCellDomElem = $(this.boardDomElem).find(".intersection[row='"+row+"'][col='"+col+"']");
+            cellColor = this.board[row][col];
+
+            if( cellColor === gogame.EMPTY && !currentCellDomElem.hasClass("empty") ){
+                emptyIntersections.push(currentCellDomElem[0] );
+            } else if( cellColor === gogame.WHITE && !currentCellDomElem.hasClass("white") ) {
+                whiteIntersections.push(currentCellDomElem[0]);
+            } else if( cellColor === gogame.BLACK && !currentCellDomElem.hasClass("black") ) {
+                blackIntersectons.push(currentCellDomElem[0]);
+            }
+        }
+    }
+
+    $(emptyIntersections).removeClass("white black").addClass("empty");
+    $(whiteIntersections).removeClass("empty black").addClass("white");
+    $(blackIntersectons).removeClass("white empty").addClass("black");
+
+    console.log("emptyIntersections",emptyIntersections);
+    console.log("whiteIntersections",whiteIntersections);
+    console.log("blackIntersectons",blackIntersectons);
+};
+
 
 /*
 gogame.Intersection = function(){
@@ -76,12 +161,19 @@ gogame.switch_player = function() {
         this.current_color == gogame.BLACK ? gogame.WHITE : gogame.BLACK;
 };
 
+gogame.get_current_player_name = function() {
+    return this.current_color == gogame.BLACK ? "Black" : "White";
+};
+
 /*
  * At any point in the game, a player can pass and let his opponent play
  */
 gogame.pass = function() {
-    if (this.last_move_passed)
+
+    if ( this.last_move_passed ) {
         this.end_game();
+    }
+
     this.last_move_passed = true;
     this.switch_player();
 };
@@ -94,16 +186,18 @@ gogame.end_game = function() {
 };
 
 /*
- * Attempt to place a stone at (i,j). Returns true iff the move was legal
+ * Attempt to place a stone at (row,col). Returns true iff the move was legal
  */
 gogame.play = function(row, col) {
-
+    var currentPlayerName = this.get_current_player_name();
+    console.log("Player :"+currentPlayerName);
     console.log("Played at " + row + ", " + col);
     this.attempted_suicide = this.in_atari = false;
 
     //invalid move
-    if (this.board[row][col] != gogame.EMPTY)
+    if ( this.board[row][col] != gogame.EMPTY ) {
         return false;
+    }
 
     var color = this.board[row][col] = this.current_color;
     var captured = [];
@@ -117,27 +211,33 @@ gogame.play = function(row, col) {
         var state = self.board[n[0]][n[1]];
 
         if (state != gogame.EMPTY && state != color) {
+
             var group = self.get_group(n[0], n[1]);
             console.log(group);
-            if (group["liberties"] == 0)
+
+            if ( group["liberties"] == 0 ) {
                 captured.push(group);
-            else if (group["liberties"] == 1)
+            } else if (group["liberties"] == 1) {
                 atari = true;
+            }
         }
     });
 
     // detect suicide
-    if (_.isEmpty(captured) && this.get_group(i, j)["liberties"] == 0) {
-        this.board[i][j] = gogame.EMPTY;
+    if ( captured.length==0 && this.get_group(row, col)["liberties"] == 0) {
+        this.board[row][col] = gogame.EMPTY;
         this.attempted_suicide = true;
         return false;
     }
 
     var self = this;
-    _.each(captured, function(group) {
-        _.each(group["stones"], function(stone) {
+    $(captured).each(function(index,group) {
+
+        //loop over each stone in the group
+        $(group["stones"]).each( function(index, stone) {
             self.board[stone[0]][stone[1]] = gogame.EMPTY;
         });
+
     });
 
     if (atari)
@@ -149,54 +249,63 @@ gogame.play = function(row, col) {
 };
 
 /*
- * Given a board position, returns a list of [i,j] coordinates representing
+ * Given a board position, returns a list of [row,col] coordinates representing
  * orthagonally adjacent intersections
  */
-gogame.get_adjacent_intersections = function(i , j) {
+gogame.get_adjacent_intersections = function(row , col) {
     var neighbors = [];
-    if (i > 0)
-        neighbors.push([i - 1, j]);
-    if (j < this.size - 1)
-        neighbors.push([i, j + 1]);
-    if (i < this.size - 1)
-        neighbors.push([i + 1, j]);
-    if (j > 0)
-        neighbors.push([i, j - 1]);
+    if (row > 0)
+        neighbors.push([row - 1, col]);
+    if (col < this.size - 1)
+        neighbors.push([row, col + 1]);
+    if (row < this.size - 1)
+        neighbors.push([row + 1, col]);
+    if (col > 0)
+        neighbors.push([row, col - 1]);
     return neighbors;
 };
 
 /*
- * Performs a breadth-first search about an (i,j) position to find recursively
+ * Performs a breadth-first search about an (row,col) position to find recursively
  * orthagonally adjacent stones of the same color (stones with which it shares
  * liberties). Returns null for if there is no stone at the specified position,
  * otherwise returns an object with two keys: "liberties", specifying the
- * number of liberties the group has, and "stones", the list of [i,j]
+ * number of liberties the group has, and "stones", the list of [row,col]
  * coordinates of the group's members.
  */
-gogame.get_group = function(i, j) {
+gogame.get_group = function(row, col) {
 
-    var color = this.board[i][j];
-    if (color == gogame.EMPTY)
+    var color = this.board[row][col];
+
+    if (color == gogame.EMPTY) {
         return null;
+    }
 
     var visited = {}; // for O(1) lookups
     var visited_list = []; // for returning
-    var queue = [[i, j]];
+    var queue = [[row, col]];
     var count = 0;
 
     while (queue.length > 0) {
         var stone = queue.pop();
+
         if (visited[stone])
             continue;
 
         var neighbors = this.get_adjacent_intersections(stone[0], stone[1]);
         var self = this;
-        _.each(neighbors, function(n) {
+        $(neighbors).each(function(index,n) {
+
             var state = self.board[n[0]][n[1]];
-            if (state == gogame.EMPTY)
+
+            if ( state == gogame.EMPTY ) {
                 count++;
-            if (state == color)
+            }
+
+            if (state == color) {
                 queue.push([n[0], n[1]]);
+            }
+
         });
 
         visited[stone] = true;
